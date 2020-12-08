3ef2bf65dec240798fde4700723508fb7c4e78e2
asa-home <sagaidaalexandr@gmail.com>
2020-12-08 23:35:39 +0200

    201208-4 generics ParametrizedClass;

--- src/main/java/generics/Test1.java
+++ src/main/java/generics/Test1.java
@@ -1,4 +1,28 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+//        list.add("ok");
+//        list.add(5);
+//        list.add(new StringBuilder("Qwerty"));
+//        list.add(new Car());
+        list.add("privet");
+        list.add("poka");
+        list.add("ok");
+        list.add("qwerty");
+
+
+        for (Object s: list) {
+            System.out.println(s + " dlinna " + ((String)s).length());
+        }
+    }
+}
+
+class Car {
+
 }
